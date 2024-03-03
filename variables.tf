e03ec323ed1ee499de739d576da83b8d6aefd3db
natarajk49 <natarajk49@gmail.com>
2024-03-03 15:08:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,15 +17,23 @@ variable "availability_zone" {
   description = "Availability zone for the subnet"
   default     = "ap-south-1b"
 }
- 
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  default     = "172.31.102.0/24"
+}
 variable "vpc_name" {
   description = "Name tag for VPC"
-  default     = "MyVPC"
+  default     = "VPC"
 }
  
 variable "public_subnet_name" {
   description = "Name tag for public subnet"
   default     = "PublicSubnet"
+}
+variable "private_subnet_name" {
+  description = "Name tag for public subnet"
+  default     = "privateSubnetA"
 }
  
 variable "public_route_table_name" {
@@ -40,16 +48,16 @@ variable "security_group_name" {
  
 variable "ec2_ami" {
   description = "AMI ID for EC2 instance"
-  default     = "ami-0449c34f967dbf18a"
+  default     = "ami-03bb6d83c60fc5f7c"
 }
  
 variable "ec2_instance_type" {
   description = "Instance type for EC2 instance"
-  default     = "t2.large"
+  default     = "c5a.xlarge"
 }
  
 variable "ec2_instance_name" {
   description = "Name tag for EC2 instance"
-  default     = "MyEC2Instance"
+  default     = "Tickerfeed"
 }
  
