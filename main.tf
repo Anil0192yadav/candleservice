a72123f89388255aee76c93d886e8dbe67326ac5
natarajk49 <natarajk49@gmail.com>
2024-03-03 17:23:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "aws_route_table" "private-crt" {
     
     route {
         cidr_block = "0.0.0.0/0"
-        nat_gateway_id = "${aws_nat_gateway.prod-nat-gateway.id}" 
+        nat_gateway_id = aws_nat_gateway.prod-nat-gateway.id
     }
     
     tags = {
@@ -94,8 +94,8 @@ resource "aws_security_group" "my_security_group" {
   vpc_id = aws_vpc.my_vpc.id
  
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -113,7 +113,20 @@ resource "aws_security_group" "my_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- 
+
+  ingress {
+    from_port   = 2022
+    to_port     = 2022
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+  ingress {
+    from_port   = 6660
+    to_port     = 6662
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+
   egress {
     from_port   = 0
     to_port     = 0
