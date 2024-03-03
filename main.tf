3ee52f5ec52242f6b9d9134eef78592f78cc00a3
natarajk49 <natarajk49@gmail.com>
2024-03-03 15:27:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "public_subnet" {
     Name = var.public_subnet_name
   }
 }
- 
+
 # Create internet gateway
 resource "aws_internet_gateway" "my_igw" {
   vpc_id = aws_vpc.my_vpc.id
@@ -44,7 +44,48 @@ resource "aws_route_table_association" "public_route_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
- 
+
+
+resource "aws_subnet" "private_subnet" {
+    vpc_id = aws_vpc.my_vpc.id
+    cidr_block = "172.31.102.0/24"
+    availability_zone = "ap-south-1b"
+    tags = {
+        Name = "private_subnet"
+    }
+}
+resource "aws_route_table" "private-crt" {
+    vpc_id = aws_vpc.my_vpc.id
+    
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = "${aws_nat_gateway.prod-nat-gateway.id}" 
+    }
+    
+    tags = {
+        Name = "private-crt"
+    }
+}
+resource "aws_route_table_association" "prod-crta-private-subnet-1"{
+    subnet_id = aws_subnet.private_subnet.id
+    route_table_id = aws_route_table.private-crt.id
+}
+resource "aws_eip" "nat_gateway" {
+    vpc = true
+}
+
+resource "aws_nat_gateway" "prod-nat-gateway" {
+    allocation_id = aws_eip.nat_gateway.id
+    subnet_id     = aws_subnet.public_subnet.id
+
+    tags = {
+    Name = "NAT Gateway"
+    }
+
+    # To ensure proper ordering, add Internet Gateway as dependency
+    depends_on = [aws_internet_gateway.my_igw]
+}
+
 # Create security group
 resource "aws_security_group" "my_security_group" {
   name        = var.security_group_name
